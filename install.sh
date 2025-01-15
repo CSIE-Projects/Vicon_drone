c54bd60d08bdbdb77d1f2ff06eeb1ff75d2caa5b
Arthur <94752718+REVOllUTiON@users.noreply.github.com>
2025-01-15 22:17:22 +0400

    Added option to autorun

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-sudo ./service_creator.sh
+# Check if the first argument is "--autorun"
+if [[ "$1" == "--autorun" ]]; then
+    sudo ./service_creator.sh
+fi
+
+# Proceed with the rest of your script
 PATH="${PATH:+${PATH}:}$HOME/.local/bin"
 pip install MAVProxy
 pip install -r requirements.txt
